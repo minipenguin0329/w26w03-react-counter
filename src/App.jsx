295061887505a299834235a587ce38f0564fdab3
minipenguin0329 <hlmingi1133@gmail.com>
2026-09-21 18:19:12 +0900

    favicon 추가

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,17 @@ import { useState } from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
-  const plusOne = (prev: number) => prev + 1
-
+  
   return (
     <>
       <h1>{count}</h1>
-      <button
-        onClick={() => {
-          setCount(plusOne)
-          setCount(plusOne)
-          setCount(plusOne)
-          setCount(plusOne)
-          setCount(plusOne)
-        }}
-      >
+      <button onClick={ () => {
+        setCount(prev => prev + 1)
+        setCount(prev => prev + 1)
+        setCount(prev => prev + 1)
+        setCount(prev => prev + 1)
+        setCount(prev => prev + 1)
+      } }>
         증가
       </button>
     </>
